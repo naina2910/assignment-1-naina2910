02005202e878e6a22ce71d9860c0357a35168b16
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-02-23 20:36:48 +0530

    fields added in person class

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,5 +1,11 @@
 package person;
 
-public class Person {
+import java.math.BigInteger;
+import java.util.ArrayList;
 
+public class Person {
+    private String fname;
+    private String lname;
+    private ArrayList<BigInteger> contactnumber;
+    private String email;
 }
