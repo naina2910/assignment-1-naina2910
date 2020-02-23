fb756e246463e73c8cd94746ed43fd868dbd98bd
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-02-23 21:10:40 +0530

    add view() method

--- src/linklist/ContactList.java
+++ src/linklist/ContactList.java
@@ -2,6 +2,9 @@ package linklist;
 
 import person.Person;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class ContactList {
     private Node head;
 
@@ -36,6 +39,42 @@ public class ContactList {
         Node node = new Node(data);
         node.setNext(head);
         head = node;
+    }
+
+    //to view all contact
+    public void view()
+    {
+        if(!isEmpty())
+        {
+            System.out.println("---Here are all your contacts---");
+            ArrayList<String> nameList = new ArrayList<>();
+            Node temp = head;
+            while (temp != null)
+            {
+                nameList.add((temp.getData().getFname()+" "+temp.getData().getLname()));
+                temp = temp.getNext();
+            }
+            Collections.sort(nameList);
+            for (String s : nameList)
+            {
+                temp = head;
+                while (temp != null)
+                {
+                    String name=temp.getData().getFname()+" "+temp.getData().getLname();
+                    if (s.equals(name))
+                    {
+                        System.out.println("-------- * -------- * -------- * -------- * -------- * --------");
+                        System.out.print(temp.getData());
+                        System.out.println("-------- * -------- * -------- * -------- * -------- * --------");
+                    }
+                    temp = temp.getNext();
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Contact list is empty");
+        }
     }
 
 
