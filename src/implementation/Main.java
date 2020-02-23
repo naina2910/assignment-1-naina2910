f7762e91cd5e299101f94a9510a84417d8963fc7
Naina Agrawal <naina.agrawal_cs18@gla.ac.in>
2020-02-23 20:47:33 +0530

    head ref in ContactList class

--- src/implementation/Main.java
+++ src/implementation/Main.java
@@ -1,4 +1,5 @@
 package implementation;
 
 public class Main {
+
 }
--- src/linklist/ContactList.java
+++ src/linklist/ContactList.java
@@ -1,4 +1,20 @@
 package linklist;
 
 public class ContactList {
+    private Node head;
+
+    public ContactList()
+    {
+        head = null;
+    }
+
+    public Node getHead()
+    {
+        return head;
+    }
+
+    public void setHead(Node head)
+    {
+        this.head = head;
+    }
 }
